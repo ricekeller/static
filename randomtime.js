d878c772e1fedd28cc6f55e1da5de8420daf4ceb
ricekeller <ricekeller@gmail.com>
2016-01-25 20:46:10 -0600

    Update randomtime.js

--- randomtime.js
+++ randomtime.js
@@ -2,15 +2,33 @@
 	function ()
 	{
 		var range=1;
-		var select=$("#rangeSelect").change(function(evt){
-			
+		var div=$("#result");
+		$("#rangeSelect").change(function(evt){
+
+		});
+
+		$("#refresh").click(function  (evt) {
+			div.empty();
+			for (var i = 0; i < 1000; i++)
+			{
+				generateRandomTime();
+			}
 		});
 
 		var minute = ["00", "15", "30", "45"]
 		var result = [];
 		var generateRandomTime = function ()
 		{
-			var t = getRandomInt(0, 36);
+			var end;
+			if(range===1)
+			{
+				end=40;
+			}
+			else if(range===2)
+			{
+				end=48;
+			}
+			var t = getRandomInt(0, end);
 			var h = Math.floor(t / 4);
 			var m = minute[t - h * 4];
 			h += 8;
@@ -26,15 +44,11 @@
 			}
 			var res = h + ":" + m + " " + suf;
 			result.push(res);
-			document.write(res);
-			document.write("<br>");
+			div.append("<p>"+res+"</p>");
 		};
 		var getRandomInt = function (min, max)
 		{
 			return Math.floor(Math.random() * (max - min + 1)) + min;
 		}
-		for (var i = 0; i < 100; i++)
-		{
-			generateRandomTime();
-		}
+		
 	})();
